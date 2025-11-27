48ba41800fa6b80fe93d6dbad572f48450e074e8
Adam Levit <adamlevit70@gmail.com>
2025-11-27 13:05:08 +0200

    add background image

--- app/src/main/java/com/example/pvzonline/MainActivity.kt
+++ app/src/main/java/com/example/pvzonline/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pvzonline
 
+import android.content.Intent
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,5 +20,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Find widgets in XML
+        val btnStartGame = findViewById<Button>(R.id.btnStartGame)
+
+        btnStartGame.setOnClickListener { startGame() }
+    }
+
+    // Function to start the GameActivity
+    fun startGame() {
+        val gameActivityIntent = Intent(this, GameActivity::class.java)
+        startActivity(gameActivityIntent)
     }
 }
