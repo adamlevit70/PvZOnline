5746f62f4677a8b95f62a4717f6bb2425d7121a8
adamlevit <adamlevit70@gmail.com>
2026-01-12 19:04:34 +0200

    Improve login and register fragments

--- app/src/main/java/com/example/pvzonline/RegisterFragment.kt
+++ app/src/main/java/com/example/pvzonline/RegisterFragment.kt
@@ -15,25 +15,30 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
         val emailInput = view.findViewById<EditText>(R.id.emailInputRegister)
         val usernameInput = view.findViewById<EditText>(R.id.usernameInputRegister)
         val passwordInput = view.findViewById<EditText>(R.id.passwordInputRegister)
+        val passwordConfirmInput = view.findViewById<EditText>(R.id.passwordConfirmInputRegister)
         val registerButton = view.findViewById<Button>(R.id.registerButton)
 
         registerButton.setOnClickListener {
             val email = emailInput.text.toString().trim()
-            val password = passwordInput.text.toString().trim()
             val username = usernameInput.text.toString().trim()
+            val password = passwordInput.text.toString().trim()
+            val confirmPassword = passwordConfirmInput.text.toString().trim()
 
-            onRegisterClicked(email, username, password)
+            onRegisterClicked(email, username, password, confirmPassword)
         }
     }
 
-    private fun onRegisterClicked(email: String, username: String, password: String) {
+    private fun onRegisterClicked(email: String, username: String, password: String, confirmPassword : String) {
         // Check if email and username fields are filled
-        if(email.isEmpty() || username.isEmpty() || password.isEmpty()) {
+        if(email.isEmpty() || username.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
             Toast.makeText(this.context, "Please enter all fields", Toast.LENGTH_SHORT).show()
             return
         }
 
         // Password validation
+        if(password != confirmPassword) {
+            Toast.makeText(this.context, "Passwords do not match", Toast.LENGTH_SHORT).show()
+        }
         if(password.length < 6) {
             Toast.makeText(this.context, "Password must be at least 6 characters long", Toast.LENGTH_SHORT).show()
             return
